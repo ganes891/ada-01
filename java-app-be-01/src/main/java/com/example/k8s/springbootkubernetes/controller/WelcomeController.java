64274d89d440efcbfd450c145a2cb461f98df03c
ganes891 <ganesan.kandasami@gmail.com>
2025-04-09 10:24:17 +0800

    lib and jenk changes

--- java-app-be-01/src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
+++ java-app-be-01/src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
@@ -8,7 +8,7 @@ public class WelcomeController {
 	
 	@RequestMapping("/greeting")
 	public String greeting() {
-		return "V1 ----->Welcome to CTC test microservice page ";
+		return "V1 ----->Welcome to CTC test JAVA based microservice";
 	}
 
 }
